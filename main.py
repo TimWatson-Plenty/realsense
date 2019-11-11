27a4c45f68d4ca364257c5d69004975d12854cc6
TimWatson-Plenty <twatson@plenty.ag>
2019-11-10 21:35:09 -0800

    Adding functionality

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ keyPath = "privkey.pem"
 mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
 
 mqttc.connect(awshost, awsport, keepalive=60)
-
 mqttc.loop_forever()
 
 
